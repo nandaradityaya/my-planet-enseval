32ded4107ffda2e7a0117248d49d195cd5420315
Nanda Raditya <nandaraditya80@gmail.com>
2023-01-23 13:49:45 +0700

    memperbarui post test html, css, dan javascriptnya

--- js/startClick.js
+++ js/startClick.js
@@ -1,17 +1,47 @@
 function startClick() {
   var startPostTest = document.getElementById("startPostTest");
-  var formQuestion = document.getElementById("postTestQuestion1");
+  var formQuestion = document.getElementById("quiz");
 
   startPostTest.classList.add("post-test-start");
   formQuestion.classList.remove("post-test-start");
   setTimeout(startPostTest.classList.add("d-none"), 0.2);
 }
 
-function nextQuestion() {
-  var formQuestion = document.getElementById("postTestQuestion1");
-  var formQuestionNext = document.getElementById("postTestQuestion2");
+var currentQuestion = 1;
+var totalQuestions = 2;
 
-  formQuestion.classList.add("post-test-start");
-  formQuestionNext.classList.remove("post-test-start");
-  setTimeout(formQuestion.classList.add("d-none"), 0.2);
+document.getElementById("question-" + currentQuestion).classList.add("active");
+
+// Next button
+var nextButton = document.getElementsByClassName("next-button");
+for (var i = 0; i < nextButton.length; i++) {
+  nextButton[i].addEventListener("click", function () {
+    if (currentQuestion < totalQuestions) {
+      document
+        .getElementById("question-" + currentQuestion)
+        .classList.remove("active");
+      currentQuestion++;
+      document
+        .getElementById("question-" + currentQuestion)
+        .classList.add("active");
+    } else {
+      alert("Quiz selesai!");
+    }
+  });
+}
+
+// Previous button
+var prevButton = document.getElementsByClassName("prev-button");
+for (var i = 0; i < prevButton.length; i++) {
+  prevButton[i].addEventListener("click", function () {
+    if (currentQuestion > 1) {
+      document
+        .getElementById("question-" + currentQuestion)
+        .classList.remove("active");
+      currentQuestion--;
+      document
+        .getElementById("question-" + currentQuestion)
+        .classList.add("active");
+    }
+  });
 }
