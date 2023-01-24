74248804ecf2658371fdd8b28c39c6ef9e8aa7ab
Nanda Raditya <nandaraditya80@gmail.com>
2023-01-24 14:14:20 +0700

    memperbarui post test dan menambahkan halaman finish

--- js/startClick.js
+++ js/startClick.js
@@ -24,9 +24,15 @@ for (var i = 0; i < nextButton.length; i++) {
       document
         .getElementById("question-" + currentQuestion)
         .classList.add("active");
-    } else {
-      alert("Quiz selesai!");
     }
+    // } else {
+    //   // alert("Quiz selesai!");
+    //   document
+    //     .getElementById("question-" + currentQuestion)
+    //     .classList.remove("active");
+    //   currentQuestion++;
+    //   document.getElementById("finish-question").classList.add("active");
+    // }
   });
 }
 
@@ -45,3 +51,31 @@ for (var i = 0; i < prevButton.length; i++) {
     }
   });
 }
+
+// -----------------
+
+var countDownDate = new Date().getTime() + 60000;
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML =
+    hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    timeOut();
+  }
+}, 1000);
