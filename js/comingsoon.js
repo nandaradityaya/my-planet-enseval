fab3bc69f29d0403181e44f2b5363eb4e62c8d33
Nanda Raditya <nandaraditya80@gmail.com>
2023-03-24 14:25:57 +0700

    menambahkan coming soon, sertifikat, dan waktu post test

---
+++ js/comingsoon.js
@@ -0,0 +1,17 @@
+var svgContainer = document.getElementById("comingsoon");
+var svgContainer2 = document.getElementById("comingsoon2");
+var animItem = bodymovin.loadAnimation({
+  wrapper: svgContainer,
+  animType: "svg",
+  loop: true,
+  autoplay: true,
+  path: "https://assets1.lottiefiles.com/packages/lf20_miikvucn.json",
+});
+
+var animItem = bodymovin.loadAnimation({
+  wrapper: svgContainer2,
+  animType: "svg",
+  loop: true,
+  autoplay: true,
+  path: "https://assets1.lottiefiles.com/packages/lf20_miikvucn.json",
+});
